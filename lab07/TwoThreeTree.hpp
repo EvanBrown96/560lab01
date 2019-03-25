d9de5c05b8aff80b1ca2714853019ba96dab0edc
EvanBrown96 <evan.br96@yahoo.com>
2019-03-25 14:41:17 -0500

    added exception files and initial public interface of 2-3 tree

---
+++ lab07/TwoThreeTree.hpp
@@ -0,0 +1,32 @@
+/**
+ * @author: Evan Brown
+ * @file: TwoThreeTree.hpp
+ * @date: 3/25/19
+ * @brief: specification of 2-3 tree methods
+ */
+
+#ifndef TWOTHREETREE_HPP
+#define TWOTHREETREE_HPP
+
+template <typename T>
+class TwoThreeTree{
+public:
+
+  TwoThreeTree();
+  ~TwoThreeTree();
+
+  TwoThreeTree(const TwoThreeTree<T>& copy);
+  TwoThreeTree<T>& operator=(const TwoThreeTree<T>& copy);
+
+  void insert(const T& value);
+  void remove(const T& value) throw(ValueNotFound);
+  void removeMin() throw(EmptyStructure);
+  void removeMax() throw(EmptyStructure);
+  bool find(const T& value) const;
+  T findMin() const throw(EmptyStructure);
+  T findMax() const throw(EmptyStructure);
+
+  // List<T> levelOrder() const;
+};
+
+#endif
---
+++ lab07/ValueNotFound.hpp
@@ -0,0 +1,39 @@
+/**
+ * @author: Evan Brown
+ * @file: ValueNotFound.hpp
+ * @date: 1/26/19
+ * @brief: header file for value not found exception
+ *         adapted from 560 lab 6
+ */
+
+#ifndef VALUENOTFOUND_HPP
+#define VALUENOTFOUND_HPP
+
+#include <stdexcept>
+
+template <typename A>
+class ValueNotFound: public std::logic_error{
+
+public:
+
+  /**
+   * constructor
+   * @param err_value: the value that caused the error (could not be found)
+   */
+  ValueNotFound(A err_value);
+
+  /**
+   * returns value that caused the error
+   * @return: error value
+   */
+  A getErrValue() const;
+
+private:
+
+  A err_value;
+
+};
+
+#include "ValueNotFound.cpp"
+
+#endif
