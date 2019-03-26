a5de8c498bcac05549f5a3f17e2a5d6b4a3d25ca
Evan Brown <evanbrown@ku.edu>
2019-03-26 11:38:07 -0500

    added code to read from file

---
+++ lab07/String.hpp
@@ -0,0 +1,48 @@
+/**
+ * @author: Evan Brown
+ * @file: String.hpp
+ * @date: 2/12/19
+ * @brief: header file for character wrapper class
+ */
+
+#ifndef STRING_HPP
+#define STRING_HPP
+
+#include <stdexcept>
+
+class String{
+
+public:
+
+  String(const char* string);
+  ~String();
+  String(const String& copy_cw);
+  String& operator=(const String& copy_cw);
+  String& operator=(const char* string);
+
+  bool operator==(const String& cw) const;
+  bool operator!=(const String& cw) const;
+  bool operator>(const String& cw) const;
+  bool operator<(const String& cw) const;
+  bool operator>=(const String& cw) const;
+  bool operator<=(const String& cw) const;
+
+  static int getASCII(const String& cw);
+
+  int getLength() const;
+
+  void printMaxLen(int max) const;
+  friend std::ostream& operator<<(std::ostream& stream, const String& cw);
+
+  char& operator[](int pos) throw(std::range_error);
+
+private:
+
+  void readString(const char* string);
+
+  char* string;
+  int length;
+
+};
+
+#endif
--- lab07/main.cpp
+++ lab07/main.cpp
@@ -1,19 +1,53 @@
 #include "TwoThreeTree.hpp"
+#include "QuickQueue.hpp"
+#include "InputParser.hpp"
+#include "String.hpp"
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char** argv){
 
+  // TwoThreeTree<char> ttt;
+  // ttt.insert('A');
+  // ttt.insert('L');
+  // ttt.insert('G');
+  // ttt.insert('O');
+  // ttt.insert('R');
+  // ttt.insert('I');
+  // ttt.insert('T');
+  // ttt.insert('H');
+  // ttt.insert('M');
+  // ttt.insert('S');
+  // std::cout << ttt.levelOrder() << "\n";
+
+  if(argc <= 1){
+    std::cout << "No input file provided, exiting.\n";
+    return -1;
+  }
+
+  QuickQueue<String> input_data(10);
+
+  std::ifstream f;
+  f.open(argv[1]);
+
+  try{
+    InputParser::parse(f, input_data);
+  }
+  catch(ParseError& err){
+    f.close();
+    std::cout << "Data could not be imported from " << argv[1] << "; error at position " << err.getErrPos() << "\n";
+    return -1;
+  }
+
+  f.close();
+
+  // create 2-3 tree
   TwoThreeTree<char> ttt;
-  ttt.insert('A');
-  ttt.insert('L');
-  ttt.insert('G');
-  ttt.insert('O');
-  ttt.insert('R');
-  ttt.insert('I');
-  ttt.insert('T');
-  ttt.insert('H');
-  ttt.insert('M');
-  ttt.insert('S');
+  while(!input_data.isEmpty()){
+    ttt.insert(input_data.pop()[0]);
+  }
+
   std::cout << ttt.levelOrder() << "\n";
+
   return 0;
 }
