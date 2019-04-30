48aef4950954de5f389000011477f8cfc1ab4814
Evan Brown <evanbrown@ku.edu>
2019-04-30 12:11:28 -0500

    added main and makefile

---
+++ lab12/main.cpp
@@ -0,0 +1,14 @@
+#include "Graph.hpp"
+#include "PriQueue.hpp"
+
+int main(int argc, char** argv){
+
+  // int* thing = new int*[3];
+  // thing[0] = {0, 1, 4};
+  // thing[1] = {-1, 0, 10};
+  // thing[2] = {14, 2, 0};
+  //
+  // Graph(3, thing);
+
+  return 0;
+}
